4b2b54a6c1bec0fe87b3fe07e6bd5c30bc78d38a
miss.involute <miss.involute@gmail.com>
2013-12-14 13:51:33 +0000

    Created new class to hold TreeView in a separate JFrame.

---
+++ src/ui/frames/CodeTreeViewFrame.java
@@ -0,0 +1,154 @@
+package ui.frames;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTree;
+import javax.swing.WindowConstants;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.tree.DefaultTreeCellRenderer;
+import javax.swing.tree.DefaultTreeModel;
+import model.modtree.ModOperandNode;
+import model.modtree.ModTree;
+
+/**
+ * Displays the current code in the editor as a tree view in a new frame.
+ * @author Amineri
+ */
+
+@SuppressWarnings("serial")
+public class CodeTreeViewFrame extends JFrame{
+	
+	/**
+	 * The reference to the current modfile tree structure.
+	 */
+	private ModTree modTree;
+	
+	/**
+	 * Constructs the application's main frame.
+	 * @param modTree
+	 */
+	public CodeTreeViewFrame(ModTree modTree) {
+		// instantiate frame
+		super();
+		
+		// store passed ModTree reference
+		this.modTree = modTree;
+		
+		// set title
+		this.setTitle("Tree View: " + this.modTree.getFunctionName());
+		
+		// create and lay out the frame's components
+		try {
+			this.initComponents();
+		} catch (Exception e) {
+//			Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, e);
+			e.printStackTrace();
+		}
+		
+		// re-route default closing behavior to close() method
+		this.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent evt) {
+				close();
+			}
+		});
+		
+		// adjust frame size
+		this.pack();
+		// center frame in screen
+		this.setLocationRelativeTo(null);
+		// show frame
+		this.setVisible(true);
+	}
+
+	/**
+	 * Creates and lays out the frame's components.
+	 * @throws Exception if an I/O error occurs
+	 */
+	private void initComponents() throws Exception {
+		
+		// configure content pane layout
+		Container contentPane = this.getContentPane();
+		
+		final JTree modElemTree = new JTree(this.modTree.getRoot()); 
+		JScrollPane modElemTreePane = new JScrollPane(modElemTree,
+				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+				JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		modElemTreePane.setPreferredSize(new Dimension(640, 480));
+		// configure look and feel of tree viewer
+		modElemTree.setRootVisible(false);
+		modElemTree.putClientProperty("JTree.lineStyle", "Angled");
+		modElemTree.setShowsRootHandles(false);
+		// Display alternate operand text info for opened ModOperandNodes
+		DefaultTreeCellRenderer renderer = new DefaultTreeCellRenderer() {
+			
+			@Override
+			public Component getTreeCellRendererComponent(
+									JTree tree,
+									Object value,
+									boolean sel,
+									boolean expanded,
+									boolean leaf,
+									int row,
+									boolean hasFocus) {
+
+					if(value instanceof ModOperandNode) {
+							((ModOperandNode) value ).expanded = expanded;
+							if(expanded) {
+								
+							}
+					} 
+					super.getTreeCellRendererComponent(
+									tree, value, sel,
+									expanded, leaf, row,
+									hasFocus);
+					return this;
+				}		
+		};
+		renderer.setLeafIcon(null);
+		renderer.setClosedIcon(null);
+		renderer.setOpenIcon(null);
+		modElemTree.setCellRenderer(renderer);	
+			
+		// install document listener to refresh tree on changes to the document
+		modTree.getDocument().addDocumentListener(new DocumentListener() {
+			@Override
+			public void removeUpdate(DocumentEvent evt) {
+				this.updateTree(evt);
+			}
+			@Override
+			public void insertUpdate(DocumentEvent evt) {
+				this.updateTree(evt);
+			}
+			@Override
+			public void changedUpdate(DocumentEvent evt) {
+				this.updateTree(evt);
+			}
+			/** Updates the tree views on document changes */
+			private void updateTree(DocumentEvent evt) {
+				// reset mod tree
+				((DefaultTreeModel) modElemTree.getModel()).setRoot(
+						modTree.getRoot());
+			}
+		});
+		
+		
+		contentPane.add(modElemTreePane);
+
+	}
+	
+	/**
+	 * Disposes the frame.
+	 */
+	private void close() {
+		// TODO: do some clean-up if necessary
+		this.dispose();
+	}
+
+}
