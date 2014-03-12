6228e198e4eab0cf8bc4ffce513fffd94573a282
miss.involute@gmail.com <miss.involute@gmail.com@875a7cc6-b973-63d7-702c-464eb8cef51a>
2014-03-12 20:04:49 +0000

    V0.82 * Fixed bug preventing recognition of embedded names within hex

--- trunk/src/ui/Constants.java
+++ trunk/src/ui/Constants.java
@@ -28,7 +28,7 @@ public class Constants {
 	/**
 	 * The application's version number.
 	 */
-	public static final String VERSION_NUMBER = "v0.81";
+	public static final String VERSION_NUMBER = "v0.82";
 	
 	/**
 	 * The list of authors.
